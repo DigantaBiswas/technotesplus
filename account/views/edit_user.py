ee28d0af78bad4d2c8967a899dc94ba3785668a0
Diganta <digantaprotik@gmail.com>
2021-08-10 14:49:18 +0600

    * api created for editing

---
+++ account/views/edit_user.py
@@ -0,0 +1,32 @@
+# Create your views here.
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class EditUser(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def put(self, request):
+        first_name = request.data.get("first_name")
+        last_name = request.data.get("last_name")
+        email = request.data.get("email")
+
+        modified = False
+
+        existing_user = request.user
+
+        if first_name:
+            existing_user.first_name = first_name
+            modified = True
+        if last_name:
+            existing_user.last_name = last_name
+            modified = True
+        if email:
+            existing_user.email = email
+            modified = True
+
+        if modified:
+            existing_user.save()
+
+        return Response({'message': 'User edited '})
